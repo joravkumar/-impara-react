01571e982702c3f78a783927232ba0d70c5ad18f
Jorav <joravkumar@gmail.com>
2021-03-03 10:28:23 +0530

    Initial Commit

---
+++ src/Tick.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+function Tick() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+// class Tick extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+
+export default Tick;
