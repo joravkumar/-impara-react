7f66ab0e94ec2a796eba1d576fc16d0adbc512b5
Jorav <joravkumar@gmail.com>
2021-03-11 10:56:56 +0530

    chore: lists and keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
+import Welcome from "./Welcome";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      userProfile: {
-        name: "jorav",
-        age: 23,
-        gender: "female",
-      },
+      users: ["Jorav", "Pallavi", "Gaurav", "Taaj"],
     };
   }
   render() {
-    return (
-      <div>
-        <h1>Username: {this.state.userProfile.name}</h1>
-        <h1>Age: {this.state.userProfile.age}</h1>
-        <h1>Gender: {this.state.userProfile.gender} {this.state.userProfile.gender === 'male' ? "👲": "👩‍💻"} </h1>
-      </div>
-    );
+    const element = this.state.users.map((user) => <Welcome name={user} key={user} />);
+    return <div>{element}</div>;
   }
 }
